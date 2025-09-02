eec1b1a13e155e18f9f6e62f3fb378c4c2d82302
RizwanShaikh313 <rizwan.shaikhcse2024@indoreinstitute.com>
2025-09-02 10:51:28 +0530

    Update table.c

--- table.cpp
+++ table.cpp
@@ -1,28 +1,14 @@
-//#include<stdio.h>
-//int main(){
-//	int a;
-//	printf("enter a number");
-//	scanf("%d",&a);
-//   int i=1;
-//   while (i<=10){
-//   	printf(" %d x %d =%d \n",a,i,a*i);
-//	   i=i+1;
-//	      }
-//
-//	}
 #include<stdio.h>
 int main(){
-	int a,sum=0;
+	int a;
 	printf("enter a number");
 	scanf("%d",&a);
-   int i=1;
-   while (i<=a){
-   	sum=sum+i;
-   	 i=i+1;
-  
-	  
+  int i=1;
+  while (i<=10){
+  	printf(" %d x %d =%d \n",a,i,a*i);
+	   i=i+1;
 	      }
-	       	printf(" %d",sum);
 
 	}
+
 
