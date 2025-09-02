7cfd881a5ab236518291a537eaca994e20619f49
RizwanShaikh313 <rizwan.shaikhcse2024@indoreinstitute.com>
2025-09-02 11:00:49 +0530

    Add files via upload

---
+++ sum of n.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int a,sum=0;
+	printf("enter a number");
+	scanf("%d",&a);
+   int i=1;
+   while (i<=a){
+   	sum=sum+i;
+   	 i=i+1;
+  
+	  
+	      }
+	       	printf(" %d",sum);
+
+	}
