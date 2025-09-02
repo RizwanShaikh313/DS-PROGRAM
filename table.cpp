194de9fcd72255262b8ee233f6f3b2d644e08d2b
RizwanShaikh313 <rizwan.shaikhcse2024@indoreinstitute.com>
2025-09-02 10:43:43 +0530

    Add files via upload

---
+++ table.cpp
@@ -0,0 +1,28 @@
+//#include<stdio.h>
+//int main(){
+//	int a;
+//	printf("enter a number");
+//	scanf("%d",&a);
+//   int i=1;
+//   while (i<=10){
+//   	printf(" %d x %d =%d \n",a,i,a*i);
+//	   i=i+1;
+//	      }
+//
+//	}
+#include<stdio.h>
+int main(){
+	int a,sum=0;
+	printf("enter a number");
+	scanf("%d",&a);
+   int i=1;
+   while (i<=a){
+   	sum=sum+i;
+   	 i=i+1;
+  
+	  
+	      }
+	       	printf(" %d",sum);
+
+	}
+
